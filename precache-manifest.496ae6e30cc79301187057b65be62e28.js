09ed01bc646f1719e7ab8541a998f22fc9da4dbe
sai24kumar24 <sai24kumar24@gmail.com>
2024-04-24 17:14:49 +0530

    Updates

---
+++ precache-manifest.496ae6e30cc79301187057b65be62e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd68353bd8d044f394ef3cf2767f66c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b271e3a796bc1862c744",
+    "url": "/monsters-rolodex/static/css/main.196166a5.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/monsters-rolodex/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/monsters-rolodex/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "b271e3a796bc1862c744",
+    "url": "/monsters-rolodex/static/js/main.67ffcdcb.chunk.js"
+  },
+  {
+    "revision": "4032d0a39dcdf2f5a75a",
+    "url": "/monsters-rolodex/static/js/runtime~main.c5797289.js"
+  }
+]);
